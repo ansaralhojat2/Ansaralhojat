0104e8ead95d9fc3ade3b0920dfd21bd3a0be901
Ali Akbar <aliakbaronweb@yahoo.com>
2017-04-29 23:32:31 +0430

    internet not connected back pressed

--- app/src/main/java/com/ansaralhojat/ansaralhojat/InternetNotConnectedActivity.java
+++ app/src/main/java/com/ansaralhojat/ansaralhojat/InternetNotConnectedActivity.java
@@ -1,7 +1,8 @@
 package com.ansaralhojat.ansaralhojat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class InternetNotConnectedActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class InternetNotConnectedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internet_not_connected);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), MainAppCompatActivity.class);
+        startActivity(intent);
     }
 }
