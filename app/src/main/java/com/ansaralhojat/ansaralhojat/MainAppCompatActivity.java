e23438e7577a401765766b8a648ae854753a4c62
Ali Akbar <aliakbaronweb@yahoo.com>
2017-04-29 21:19:25 +0430

    main progress bar

--- app/src/main/java/com/ansaralhojat/ansaralhojat/MainAppCompatActivity.java
+++ app/src/main/java/com/ansaralhojat/ansaralhojat/MainAppCompatActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,12 +20,15 @@ import com.android.volley.toolbox.Volley;
 import utils.JsonUtils;
 
 public class MainAppCompatActivity extends BaseAppCompatActivity {
-    ProgressBar progressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        progressBar = (ProgressBar) findViewById(R.id.pb_main);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.pb_main);
+        final LinearLayout addressLayout = (LinearLayout) findViewById(R.id.address_group);
+        final LinearLayout timeLayout = (LinearLayout) findViewById(R.id.time_group);
+        final LinearLayout familyLayout = (LinearLayout) findViewById(R.id.family_group);
+        final LinearLayout parkingLayout = (LinearLayout) findViewById(R.id.parking_group);
+        final TextView qoran = (TextView) findViewById(R.id.remember_qoran);
 
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         StringRequest requestAddress = new StringRequest(Request.Method.GET,
@@ -59,6 +63,11 @@ public class MainAppCompatActivity extends BaseAppCompatActivity {
                         final ImageView imageView = (ImageView) findViewById(R.id.main_image);
                         imageView.setImageBitmap(response);
                         progressBar.setVisibility(View.GONE);
+                        addressLayout.setVisibility(View.VISIBLE);
+                        timeLayout.setVisibility(View.VISIBLE);
+                        familyLayout.setVisibility(View.VISIBLE);
+                        parkingLayout.setVisibility(View.VISIBLE);
+                        qoran.setVisibility(View.VISIBLE);
                     }
                 },
                 1028,
